22f139936a5db095edd28a4c0b60b61985907bf1
Ruturaj Chaubey <ruturajchaubey16@gmail.com>
2021-07-23 14:23:48 +0530

    Created the login page UI

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,9 @@
 import PostsList from './PostsList';
 import Navbar from './Navbar';
 import Home from './Home';
+import Login from './Login';
 import Page404 from './Page404';
+
 import App from './App';
 
-export { PostsList, App, Navbar, Home, Page404 };
+export { PostsList, App, Navbar, Home, Page404, Login };
